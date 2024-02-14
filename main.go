0cdba045841291d35ad658a290378a40f206dcd3
Illya Katynskyi <katinskyi.illia@lll.kpi.ua>
2024-02-14 11:19:48 +0200

    Refactor code

--- main.go
+++ main.go
@@ -2,25 +2,34 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
 	"time"
 )
 
 func main() {
-	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
-		currentTime := time.Now().Format(time.RFC3339)
+	http.HandleFunc("/time", handleTime)
+	serverAddress := ":8795"
+	log.Printf("Server running on http://localhost%s", serverAddress)
+	log.Fatal(http.ListenAndServe(serverAddress, nil))
+}
 
-		response := struct {
-			Time string `json:"time"`
-		}{
-			Time: currentTime,
-		}
+func handleTime(w http.ResponseWriter, r *http.Request) {
+	currentTime := getCurrentTime()
+	response := TimeResponse{Time: currentTime}
+	respondWithJSON(w, http.StatusOK, response)
+}
 
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(response)
-	})
+func getCurrentTime() string {
+	return time.Now().Format(time.RFC3339)
+}
 
-	fmt.Println("Server running on http://localhost:8795")
-	http.ListenAndServe(":8795", nil)
+type TimeResponse struct {
+	Time string `json:"time"`
+}
+
+func respondWithJSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(data)
 }
